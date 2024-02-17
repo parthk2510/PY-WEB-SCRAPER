35b7b8e69a3b95db72fb277a1e2853cf65e538af
PARTH KULKARNI <kulkarni.pp@somaiya.edu>
2024-02-17 23:14:48 +0530

    Create Api.py

---
+++ Api_Scraper/Api.py
@@ -0,0 +1,42 @@
+import requests
+import xlwt
+from xlwt import Workbook
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+# https://myaccount.google.com/lesssecureapps
+BASE_URL = 'https://remoteok.com/api/'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+REQUEST_HEADER = {
+    'User-Agent': USER_AGENT,
+    'Accept-Language': 'en-US, en;q=0.5',
+}
+
+
+def get_job_postings():
+    res = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
+    return res.json()
+
+
+def output_jobs_to_xls(data):
+    wb = Workbook()
+    job_sheet = wb.add_sheet('Jobs')
+    headers = list(data[0].keys())
+    for i in range(0, len(headers)):
+        job_sheet.write(0, i, headers[i])
+    for i in range(0, len(data)):
+        job = data[i]
+        values = list(job.values())
+        for x in range(0, len(values)):
+            job_sheet.write(i+1, x, values[x])
+    wb.save('remote_jobs.xls')
+
+
+if __name__ == "__main__":
+    json = get_job_postings()[1:]
+    output_jobs_to_xls(json)
+    
